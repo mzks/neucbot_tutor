5ee5c9bfae0c09d3cf3fb6bb9439405d1d2cd505
Mizukoshi Keita <mzks@km.phys.sci.osaka-u.ac.jp>
2017-11-12 14:08:33 +0900

    add some command for help

--- help1.sh
+++ help1.sh
@@ -3,8 +3,15 @@
 echo This is a shell script to help lecture.
 
 git clone https://github.com/shawest/neucbot.git
-
 mv neucbot ../neucbot
 cd ..
 mv neucbot_tutor neucbot
+cd neucbot
+cd Data/Isotopes
+git clone https://github.com/neucbot-datasets/fe.git
+tar xzf fe/Fe.tar.gz 
+cd ../../
+echo fe 0 100 > Materials/Iron.dat
+cat Materials/Iron.dat               
+./neucbot.py -c Chains/U238upperChain.dat -m Materials/Iron.dat -o iron_u.dat
 
